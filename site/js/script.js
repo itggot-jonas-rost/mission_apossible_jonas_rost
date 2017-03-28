849437d5af86d7a2f2858669397e4e0c93b39952
itggot-jonas-rost <jonas.rost@itggot.se>
2017-03-28 15:27:47 +0200

    Gjort klart alla sidor för Iphone 6

--- site/js/script.js
+++ site/js/script.js
@@ -1,9 +1,37 @@
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "67%";
 }
 
-/* Set the width of the side navigation to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+var modal = document.getElementById('myModal');
+
+var span = document.getElementsByClassName("close")[0];
+
+var button = document.getElementById("close_button");
+
+
+function modal_popup() {
+     modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+button.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
